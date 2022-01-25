50e488d468a779dd3a70909514d6736d4e67089b
Andy Hunt <andyhunt353@gmail.com>
2022-01-25 12:33:51 +0000

    Add select for color format

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,26 @@
 import React, { PureComponent } from 'react'
 import Slider from 'rc-slider';
+import { Select, MenuItem } from '@material-ui/core';
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
 
 class Navbar extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            format: 'hex'
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        const { value } = e.target
+        this.setState({ format: value});
+        this.props.changeFormat(value);
+    }
+
     render() {
         const { hue, changeHue } = this.props;
         return (
@@ -16,7 +33,16 @@ class Navbar extends PureComponent {
                     <div className='slider'>
                         <Slider defaultValue={hue} min={100} max={900} onAfterChange={changeHue} step={100}/>
                     </div>
-                </div>      
+                </div>   
+                <div>
+                    <div className='select-container'>
+                        <Select value={this.state.format} onChange={this.handleChange}>
+                            <MenuItem value='hex'>Hex - #FFF</MenuItem>
+                            <MenuItem value='rgb'>RGB - 255, 255, 255</MenuItem>
+                            <MenuItem value='rgba'>RGBA - 255, 255, 255, 1.0</MenuItem>
+                        </Select>
+                    </div>
+                </div>   
             </header>
         )
     }
--- src/Pallette.js
+++ src/Pallette.js
@@ -8,25 +8,31 @@ class Pallette extends PureComponent {
         super(props)
 
         this.state = {
-            paletteHue: 400 
+            paletteHue: 400,
+            format: 'hex'
         }
 
         this.changeHue = this.changeHue.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     changeHue = (hue) => {
         this.setState({ paletteHue: hue});
     }
 
+    changeFormat = (format) => {
+        this.setState({ format: format });
+    }
+
     render() {
-        const { paletteHue } = this.state
+        const { paletteHue, format } = this.state
         const {colors } = this.props.palette
         return (
             <div className="pallette">
-               <Navbar hue={paletteHue} changeHue={this.changeHue}/>
+               <Navbar hue={paletteHue} changeHue={this.changeHue} changeFormat={this.changeFormat} />
                 <div className='pallette-colors'>
                     {colors[paletteHue].map(color => {
-                        return <ColorBox name={color.name} color={color.hex} />
+                        return <ColorBox name={color.name} color={color[format]} />
                     })}
                 </div>
             </div>
