b3ee8f2b02da15c895e7394fbc3d36968ea36322
Andy Hunt <andyhunt353@gmail.com>
2022-01-25 13:05:52 +0000

    Add snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,10 @@
 import React, { PureComponent } from 'react'
 import Slider from 'rc-slider';
-import { Select, MenuItem } from '@material-ui/core';
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
 
@@ -8,21 +12,28 @@ class Navbar extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            format: 'hex'
+            format: 'hex',
+            open: false
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
 
     handleChange = (e) => {
         e.preventDefault();
         const { value } = e.target
-        this.setState({ format: value});
+        this.setState({ format: value, open: true}, ()=> {});
         this.props.changeFormat(value);
+    }
+
+    closeSnackbar = () => {
+        this.setState ({ open: false})
     }
 
     render() {
         const { hue, changeHue } = this.props;
+        const { format } = this.state;
         return (
             <header className='navbar'>
                 <div className='logo'>
@@ -36,13 +47,37 @@ class Navbar extends PureComponent {
                 </div>   
                 <div>
                     <div className='select-container'>
-                        <Select value={this.state.format} onChange={this.handleChange}>
+                        <Select value={format} onChange={this.handleChange}>
                             <MenuItem value='hex'>Hex - #FFF</MenuItem>
                             <MenuItem value='rgb'>RGB - 255, 255, 255</MenuItem>
                             <MenuItem value='rgba'>RGBA - 255, 255, 255, 1.0</MenuItem>
                         </Select>
                     </div>
-                </div>   
+                </div>  
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={
+                        <span id='message-id'>
+                        Format Changed To {format.toUpperCase()}
+                        </span>
+                    }
+                    ContentProps={{
+                        "aria-describedby": "message-id"
+                    }}
+                    onClose={this.closeSnackbar}
+                    action={[
+                        <IconButton
+                        onClick={this.closeSnackbar}
+                        color='inherit'
+                        key='close'
+                        aria-label='close'
+                        >
+                        <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
             </header>
         )
     }
